0b1b6183588d849d72b934ad8d6524e61ee91abf
deyvisson eduardo <deyvissoneduardo22@gmail.com>
2020-10-18 15:09:41 -0300

    saving process by date

--- process_in_memory.sh
+++ process_in_memory.sh
@@ -6,5 +6,6 @@ list_processo=$(ps -e -o pid --sort -size | head -n 11 | grep [0-9])
 for pid in $list_process
 do
 	# lista nomes do processo
-	echo $(ps -p $pid -o comm=)
+	name_process=$(ps -p $pid -o comm=)
+	echo -n $(date +%F, %H:%M:%S,) >> $name_process.log
 done
